1c8e808bc4d34c6632b53c22cdbdc66a574ad238
Islam-Turky <iturky06@gmail.com>
2024-03-07 17:10:57 +0200

    continued

--- server/V1/Helpers/AdminValidator.js
+++
@@ -1,8 +0,0 @@
-const User = require('../Config//Schemas/UserSchema');
-
-const isAdmin = async (email) => {
-    const check = await User.findOne({email: email, role: "admin"});
-    return check;
-}
-
-module.exports = isAdmin;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-require('./V1/V1Server');
+// Call Express Api.
+const express = require('express')
+const app = express();
+
+let visitor = 0;
+function increment(req, res, next) {
+    if(req){
+        visitor++;
+    }
+}
+app.get('/', increment, function(req, res) {
+    if(req){
+        res.write(`<h1>${visitor}</h1>`);
+    }
+})
+app.listen(3000, function () {
+	console.log("Express Started on Port 3000");
+});
